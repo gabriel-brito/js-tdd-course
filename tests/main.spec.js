0d10727ba838c1547b8481ed061c3c2ba14d3b0a
Gabriel <mfkrgabriel@gmail.com>
2018-03-01 14:51:14 -0300

    Começando a desenvolver testes na prática

---
+++ tests/main.spec.js
@@ -0,0 +1,13 @@
+describe('Main', function(){
+
+  describe('method 1', function(){
+    context('Case 1', function(){
+
+      it('should happen bla bla bla', function(){
+
+      });
+
+    });
+  });
+
+});
